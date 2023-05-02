de3103aa3c85fd8f5503b43c5b23c2634b00adfd
ahmedboomdo <132241370+ahmedboomdo@users.noreply.github.com>
2023-05-02 12:45:59 +1200

    Signup page created

--- Gym-Web/routes.py
+++ Gym-Web/routes.py
@@ -7,8 +7,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html", title = "Home")
+    return render_template("home.html", title = "Home Page")
 
+@app.route("/Signup")
+def signup():
+    return render_template("signup.html",title = "Sign up")
 
 if __name__ == '__main__':
     app.run(debug = True)
